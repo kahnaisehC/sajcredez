2e0edbeb2a1775ff9cd9b48d91959b13d3e501fe
Ian Hakanson <gabianhak@gmail.com>
2025-07-27 01:03:49 -0300

    feat: complete interfaces, structs and constants

--- pkg/board.go
+++ pkg/board.go
@@ -1,17 +1,90 @@
 package board
 
+type Piece int8
+
+const (
+	EMPTY Piece = iota
+	WPAWN
+	WKNIGHT
+	WBISHOP
+	WROOK
+	WKING
+	BPAWN
+	BKNIGHT
+	BBISHOP
+	BROOK
+	BKING
+)
+
+var default_board = [7][7]Piece{
+	{WROOK, WKNIGHT, WBISHOP, WKING, WBISHOP, WKNIGHT, WROOK},
+	{WPAWN, WPAWN, WPAWN, WPAWN, WPAWN, WPAWN, WPAWN},
+	{EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+	{EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+	{EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+	{BPAWN, BPAWN, BPAWN, BPAWN, BPAWN, BPAWN, BPAWN},
+	{BROOK, BKNIGHT, BBISHOP, BKING, BBISHOP, BKNIGHT, BROOK},
+}
+
 type Options struct{}
 
-type Square int
+type Color bool
 
-type Move struct{}
+const (
+	WHITE Color = false
+	BLACK Color = true
+)
+
+type Enhance int8
+
+const (
+	NO_ENHANCE Enhance = (iota)
+	ENHANCE_MOVEMENT
+)
+
+type Square struct {
+	col int8
+	row int8
+}
+
+type Move struct {
+	from      Square
+	to        Square
+	fromPiece Piece
+	toPiece   Piece
+
+	enhancement Enhance
+	promotion   Piece
+}
 
 type Board interface {
-	GetListOfMoves(options Options) []Move
-	MakeMove(Move) error
 	GetBoardString() string
+	GetHistoryString() string
+
+	GetPieceColor(Piece) Color
+	GetPiece(Square) Piece
+	SquareIsThreattened(Color) []Square
+	CheckMoveLegality(Move) error
+
+	GetListOfMoves() []Move
+	MakeMove(Move) error
 }
 
 type Sajcredez struct {
-	board [7][7]int
+	History []Move
+
+	board         [7][7]Piece
+	whiteEnhances Enhance
+	blackEnhances Enhance
+	turn          Color
+
+	BlackKingCastle  bool
+	BlackQueenCastle bool
+	WhiteKingCastle  bool
+	WhiteQueenCastle bool
+
+	// Make move skips move validation
+	// in order to prevent having to check if a move is legal
+	// for engines or uis
+	skipValidation bool
 }
